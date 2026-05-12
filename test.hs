6254a6dd43349556188d44cd14b2c21c9bed93d1
orca <35243139+thennothinghappened@users.noreply.github.com>
2026-05-12 16:49:09 +1000

    refactor: cleaner whitespace lexing

--- test.hs
+++ test.hs
@@ -1,10 +1,11 @@
 {-# LANGUAGE ViewPatterns #-}
 
-import Prelude (String, Int, IO, readFile, putStrLn, Monad ((>>=)), Show (show), pure, mapM_, print, Maybe (Just, Nothing), Eq ((==)), Char)
+import Prelude (String, Int, IO, readFile, putStrLn, Monad ((>>=)), Show (show), pure, mapM_, print, Maybe (Just, Nothing), Eq ((==)), Char, undefined)
 import Data.List
 import Data.Bool
 import Data.Char (isAlphaNum, isAlpha, isNumber)
 import Text.Read (readMaybe)
+import Unicode.Char (isWhiteSpace)
 
 data Token =
     Ident String
@@ -20,27 +21,38 @@ main = do
 lex :: String -> [Token]
 lex [] = []
 lex ('=' : rest) = SingleEquals : lex rest
-lex (' ' : rest) = lex rest
-lex ('\n' : rest) = lex rest
-lex ('\t' : rest) = lex rest
 lex input
-  | isAlpha (head input) || (head input == '_') =
-    let (ident, rest) = consumeIdent input in
-      Ident ident : lex rest
-  | isNumber (head input) = let (value, rest) = consumeInt input in
-      IntLit value : lex rest
+  | isValidIdentStart c = lexIdent input
+  | isNumber c = lexInt input
+  | isWhiteSpace c = lex (dropWhile isWhiteSpace input)
   | otherwise = []
+  where
+    c = head input
+
+lexIdent :: String -> [Token]
+lexIdent input = Ident ident : lex rest
+  where (ident, rest) = consumeIdent input
+
+lexInt :: String -> [Token]
+lexInt input = IntLit value : lex rest
+  where (value, rest) = consumeInt input
 
 consumeIdent :: String -> (String, String)
 consumeIdent input
-  | isAlphaNum (head input) || (head input == '_') = span isValidIdent input
+  | isAlphaNum c || (c == '_') = span isValidIdent input
   | otherwise = ([], input)
+  where
+    c = head input
 
 consumeInt :: String -> (Int, String)
-consumeInt input = let (numString, rest) = span isNumber input in
-  case readMaybe numString of
-    Just value -> (value, rest)
-    Nothing -> (0, rest)
+consumeInt input =
+  let (numString, rest) = span isNumber input
+    in case readMaybe numString of
+      Just value -> (value, rest)
+      Nothing -> undefined
 
 isValidIdent :: Char -> Bool
 isValidIdent c = isAlphaNum c || (c == '_')
+
+isValidIdentStart :: Char -> Bool
+isValidIdentStart c = isAlpha c || (c == '_')
